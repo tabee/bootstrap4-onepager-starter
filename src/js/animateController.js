5d755d4bad8e583144f0d8d4fdce968b53b4f400
tabee <mario.bee@stud.hslu.ch>
2017-02-23 23:44:29 +0100

    listen mouse events an fires animations.

---
+++ src/js/animateController.js
@@ -0,0 +1,27 @@
+// welcome animation over all
+$(document).ready(function () {
+    $('html').animateCss('bounce');
+});
+
+// A tada animation for all fontawesome icons.
+$( ".fa" ).hover( function() {
+    $(this).animateCss('tada'); // Issue: it breaks fontawesome default animation after hover.
+});
+
+// A zoomOut animation for all href's
+$("a").on('click', function () {
+    $(this).animateCss('zoomOut');
+    $(this).hide('slow').delay(200).show('slow');
+});
+
+// helper from fontawesome to add needed classes.
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
+
